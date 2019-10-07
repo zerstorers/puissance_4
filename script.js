881c26c4b9dc17af154e0c9fe7681a951ede0b5f
a-delmas <aurore.delmas@hotmail.fr>
2019-10-07 12:28:37 +0200

    Switch 1

--- script.js
+++ script.js
@@ -7,27 +7,29 @@ document.body.appendChild(tableau);
 var button = document.createElement("div");
 // Creation du tableau
 
-for (let btn = 0; btn < 7; btn++){
-    var button = document.createElement("div");
-    div.appendChild(button)
-    button.setAttribute("class", "div1")
-    button.id = btn
-}
-
 for (let i = 0; i < 6; i++) {
     var tr = document.createElement("tr");
     tableau.appendChild(tr);
     for (let j = 0; j < 7; j++) {
         var td = document.createElement("td");
         tr.appendChild(td);
-        td.id = j + "-" + i;
+        td.id = i + "-" + j;
     }
 }
 
 // Fin creation tableau
 
-button.addEventListener("clcik", function(){
-    var pion = document.createElement("div");
-    td.appendChild(pion)
-    pion.id = pion
-})
+for (let btn = 0; btn < 7; btn++) {
+    var button = document.createElement("div");
+    div.appendChild(button)
+    button.setAttribute("class", "div1")
+    button.id = btn
+    button.addEventListener("click", function () {
+        var colon_num = this.id
+        div [id $ = "0"]
+        this.querySelectorAll("0")
+    })
+}
+
+
+
